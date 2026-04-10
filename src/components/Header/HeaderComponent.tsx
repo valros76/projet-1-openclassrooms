75359af6ff18cc305664d24eedecc127b1450104
Valérian Dufrène <valros76@gmail.com>
2026-04-10 15:42:11 +0200

    Fix logo crash at page refresh

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,9 +1,10 @@
 import "./HeaderComponent.css";
+import logo from "/assets/images/logo/logo.svg";
 
 export const HeaderComponent = () => {
   return (
     <header className="main-head max-w-6xl mx-auto">
-      <img src="assets/images/logo/logo.svg" alt="logo télésport" className="logo"/>
+      <img src={logo} alt="logo télésport" className="logo"/>
       <h1 className="main-head-title text-4xl font-bold">
         Historique des Jeux Olympiques - TéléSport
       </h1>
